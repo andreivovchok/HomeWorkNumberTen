51571043241b31ddbd70b456190467b008400733
Andrei Vovchok <andreivovchok@bk.ru>
2021-08-24 19:39:40 +0300

    Добавил решение задания

---
+++ HomeWorkNumberTen/MyItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWorkNumberTen
+{
+    public class MyItem
+    {
+        public string Item { get; set; }
+
+        public override string ToString()
+        {
+            return "Item: " + Item;
+        }
+    }
+}
---
+++ HomeWorkNumberTen/MyList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HomeWorkNumberTen
+{
+    public class MyList : IList<MyItem>
+    {
+        private List<MyItem> _myItems = new List<MyItem>();
+
+        public MyItem this[int index] 
+        {
+            get { return _myItems[index]; }
+            set { _myItems[index] = value; }
+        }
+
+        public int Count 
+        { 
+            get { return _myItems.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public void Add(MyItem item)
+        {
+            _myItems.Add(item);
+        }
+
+        public void Clear()
+        {
+            _myItems.Clear();
+        }
+
+        public bool Contains(MyItem item)
+        {
+            return _myItems.Contains(item);
+        }
+
+        public void CopyTo(MyItem[] array, int arrayIndex)
+        {
+            _myItems.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<MyItem> GetEnumerator()
+        {
+            return _myItems.GetEnumerator();
+        }
+
+        public int IndexOf(MyItem item)
+        {
+            return _myItems.IndexOf(item);
+        }
+
+        public void Insert(int index, MyItem item)
+        {
+            _myItems.Insert(index, item);
+        }
+
+        public bool Remove(MyItem item)
+        {
+            return _myItems.Remove(item);
+        }
+
+        public void RemoveAt(int index)
+        {
+            _myItems.RemoveAt(index);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _myItems.GetEnumerator();
+        }
+    }
+}
